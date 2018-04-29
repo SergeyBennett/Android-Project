ac71a3c5e631b732b8b94355cd3665f41e04a516
IlyaSirosh <IlyaSirosh@users.noreply.github.com>
2018-04-29 20:35:51 +0300

    added redux to lists

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import NotesReducer from './reducer_notes'
 import CurrentNoteReducer from './reducer_current'
+import ListsReducer from './reducer_lists'
+
 
 const rootReducer = combineReducers({
   allNotes: NotesReducer,
-  currentNote: CurrentNoteReducer
+  currentNote: CurrentNoteReducer,
+  lists: ListsReducer
 })
 
 export default rootReducer
